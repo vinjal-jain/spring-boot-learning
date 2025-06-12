8a230e7056bfbca853b9e2fef8479acd1190cc40
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-12 13:56:53 +0530

    add OneToMany relationship for courses in Student entity

--- src/main/java/com/vinjal/starting/entity/Student.java
+++ src/main/java/com/vinjal/starting/entity/Student.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Student {
@@ -20,5 +21,6 @@ public class Student {
 
     private Integer rollNumber;
 
-    private List
+    @OneToMany(mappedBy = "student")
+    private List<Course> courses;
 }
