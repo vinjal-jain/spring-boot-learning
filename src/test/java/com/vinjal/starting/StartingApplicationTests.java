223f2ddb36b7d69163e49236ec3be2b66c842e7a
vinjal-jain <vinjaljain14it@gmail.com>
2025-06-12 13:37:25 +0530

    first commit

---
+++ src/test/java/com/vinjal/starting/StartingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinjal.starting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
